e6f98f8cfd0c9be9df0ea7bdb27c06338196a82f
Maks6831 <ma.khan6971@gmail.com>
2023-03-07 14:33:43 +0000

    set up local server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+// express is a backend web application framework.
+const app = express();
+
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('room');
+})
+
+app.get('/:room', (req, res) =>{
+    res.render('room', {roomId: req.params.room});
+} )
+
+
+
+server.listen(3030);
+// server is going to be local host - port is 3030
+
+// this is my first node js app.
